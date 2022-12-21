21b27194cb4370c649a042d42a78bc715127cf8d
Vlad <vladjdk@gmail.com>
2022-12-21 13:15:07 -0500

    fmt, clipppy

--- contracts/warp-controller/src/tests/execute/account/test_create_account.rs
+++ contracts/warp-controller/src/tests/execute/account/test_create_account.rs
@@ -1,14 +1,13 @@
-use crate::contract::{instantiate, reply};
-use crate::execute::account::create_account;
+
+
 use crate::tests::helpers::{create_warp_account, instantiate_warp};
 use crate::ContractError;
 use cosmwasm_std::testing::{mock_dependencies, mock_env, mock_info};
 use cosmwasm_std::{
-    coin, to_binary, Attribute, CosmosMsg, Event, Reply, ReplyOn, Response, SubMsg, SubMsgResponse,
-    SubMsgResult, Uint128, Uint64, WasmMsg,
+    coin, to_binary, CosmosMsg, ReplyOn, Response, SubMsg, Uint128, Uint64, WasmMsg,
 };
-use std::borrow::BorrowMut;
-use warp_protocol::controller::controller::InstantiateMsg;
+
+
 
 #[test]
 fn test_create_account_success() {
@@ -16,7 +15,7 @@ fn test_create_account_success() {
     let env = mock_env();
     let info = mock_info("vlad", &vec![coin(100, "uluna")]);
 
-    let instantiate_res = instantiate_warp(
+    let _instantiate_res = instantiate_warp(
         deps.as_mut(),
         env.clone(),
         info.clone(),
--- contracts/warp-controller/src/tests/execute/controller/test_update_config.rs
+++ contracts/warp-controller/src/tests/execute/controller/test_update_config.rs
@@ -11,7 +11,7 @@ fn test_modify_config_success() {
     let env = mock_env();
     let info = mock_info("vlad", &vec![coin(100, "uluna")]);
 
-    let instantiate_res = instantiate_warp(
+    let _instantiate_res = instantiate_warp(
         deps.as_mut(),
         env.clone(),
         info.clone(),
@@ -50,7 +50,7 @@ fn test_modify_config_unauthorized() {
     let env = mock_env();
     let info = mock_info("vlad", &vec![coin(100, "uluna")]);
 
-    let instantiate_res = instantiate_warp(
+    let _instantiate_res = instantiate_warp(
         deps.as_mut(),
         env.clone(),
         info.clone(),
@@ -83,7 +83,7 @@ fn test_modify_config_bad_percentages() {
     let env = mock_env();
     let info = mock_info("vlad", &vec![coin(100, "uluna")]);
 
-    let instantiate_res = instantiate_warp(
+    let _instantiate_res = instantiate_warp(
         deps.as_mut(),
         env.clone(),
         info.clone(),
