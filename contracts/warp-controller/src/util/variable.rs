3bae482edd23989c45008fe6234d7216c7fcd403
Vlad <vladjdk@gmail.com>
2023-01-10 15:19:57 -0500

    change 2

--- contracts/warp-controller/src/util/variable.rs
+++ contracts/warp-controller/src/util/variable.rs
@@ -141,7 +141,7 @@ pub fn hydrate_msgs(
                 }
             };
             msg = msg.replace(&format!("\"$warp.variable.{}\"", name), &replacement);
-            if replacement.contains("$warp.variable") { //todo: change to new version after merge
+            if replacement.contains("$warp.variable") {
                 return Err(ContractError::Unauthorized {}) //todo: err
             }
         }
