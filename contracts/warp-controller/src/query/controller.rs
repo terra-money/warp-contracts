7dd4ea3d5d0b00792ca652afa94d06f6cbe1e2f3
Vlad <vladjdk@gmail.com>
2023-05-29 16:54:18 -0400

    Rm util, unneeded

--- contracts/warp-controller/src/query/controller.rs
+++ contracts/warp-controller/src/query/controller.rs
@@ -1,8 +1,8 @@
 use crate::state::CONFIG;
 use crate::util::condition::resolve_query_expr;
 use cosmwasm_std::{Deps, Env, StdError, StdResult};
-use warp_protocol::controller::variable::QueryExpr;
-use warp_protocol::controller::{
+use controller::variable::QueryExpr;
+use controller::{
     ConfigResponse, QueryConfigMsg, SimulateQueryMsg, SimulateResponse,
 };
 
