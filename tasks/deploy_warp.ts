a070d92ac550ca166353721e42faeff56b0670f1
Vlad <vladjdk@gmail.com>
2023-07-20 17:58:18 -0400

    tests

--- tasks/deploy_warp.ts
+++ tasks/deploy_warp.ts
@@ -4,47 +4,52 @@ task(async ({ deployer, signer, refs }) => {
   //account
   deployer.buildContract("warp-account");
   deployer.optimizeContract("warp-account");
-  //
-  // const id = await deployer.storeCode("warp-account");
-  // await new Promise((resolve) => setTimeout(resolve, 10000));
-  //
-  // await deployer.storeCode("warp-resolver");
-  // await new Promise((resolve) => setTimeout(resolve, 10000));
-  //
-  // //controller
-  // // deployer.buildContract("warp-controller");
-  // // deployer.optimizeContract("warp-controller");
-  //
-  // await deployer.storeCode("warp-controller");
-  // await new Promise((resolve) => setTimeout(resolve, 10000));
-  // //
-  // const instantiateControllerMsg = {
-  //   warp_account_code_id: id,
-  //   creation_fee: "5",
-  //   cancellation_fee: "5",
-  //   minimum_reward: "10000",
-  //   t_max: "86400",
-  //   t_min: "86400",
-  //   a_max: "10000",
-  //   a_min: "10000",
-  //   q_max: "10",
-  // };
-  //
-  // await deployer.instantiate("warp-controller", instantiateControllerMsg, {
-  //   admin: signer.key.accAddress,
-  // });
-  // await new Promise((resolve) => setTimeout(resolve, 10000));
-  //
-  // const instantiateResolverMsg = {
-  //   owner: signer.key.accAddress,
-  //   fee_collector: signer.key.accAddress,
-  //   templates: [],
-  // }
-  //
-  // await deployer.instantiate("warp-resolver", instantiateResolverMsg, {
-  //   admin: signer.key.accAddress,
-  // });
-  // await new Promise((resolve) => setTimeout(resolve, 10000));
-  //
-  // refs.saveRefs();
+
+  const id = await deployer.storeCode("warp-account");
+  await new Promise((resolve) => setTimeout(resolve, 10000));
+
+  await deployer.storeCode("warp-resolver");
+  await new Promise((resolve) => setTimeout(resolve, 10000));
+
+  await deployer.storeCode("warp-templates");
+  await new Promise((resolve) => setTimeout(resolve, 10000));
+
+  await deployer.storeCode("warp-controller");
+  await new Promise((resolve) => setTimeout(resolve, 10000));
+
+  const instantiateTemplatesMsg = {
+    owner: signer.key.accAddress,
+    fee_collector: signer.key.accAddress,
+    templates: [],
+  }
+
+  await deployer.instantiate("warp-templates", instantiateTemplatesMsg, {
+    admin: signer.key.accAddress,
+  });
+  await new Promise((resolve) => setTimeout(resolve, 10000));
+
+  let resolver_address = await deployer.instantiate("warp-resolver", {}, {
+    admin: signer.key.accAddress,
+  });
+  await new Promise((resolve) => setTimeout(resolve, 10000));
+
+  const instantiateControllerMsg = {
+    warp_account_code_id: id,
+    creation_fee: "5",
+    cancellation_fee: "5",
+    minimum_reward: "10000",
+    resolver_address: resolver_address.address,
+    t_max: "86400",
+    t_min: "86400",
+    a_max: "10000",
+    a_min: "10000",
+    q_max: "10",
+  };
+
+  await deployer.instantiate("warp-controller", instantiateControllerMsg, {
+    admin: signer.key.accAddress,
+  });
+  await new Promise((resolve) => setTimeout(resolve, 10000));
+
+  refs.saveRefs();
 });
