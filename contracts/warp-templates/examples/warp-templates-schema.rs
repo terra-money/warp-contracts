c54e135b2ca56b502d9735e1c9aee2e80e3ff6a4
simke9445 <simke9445@gmail.com>
2023-08-17 15:45:23 +0200

    fix schemas

--- contracts/warp-templates/examples/warp-templates-schema.rs
+++ contracts/warp-templates/examples/warp-templates-schema.rs
@@ -2,7 +2,10 @@ use std::env::current_dir;
 use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
-use resolver::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use templates::{
+    template::{TemplateResponse, TemplatesResponse},
+    ConfigResponse, ExecuteMsg, InstantiateMsg, QueryMsg, Config,
+};
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
@@ -13,4 +16,8 @@ fn main() {
     export_schema(&schema_for!(InstantiateMsg), &out_dir);
     export_schema(&schema_for!(ExecuteMsg), &out_dir);
     export_schema(&schema_for!(QueryMsg), &out_dir);
+    export_schema(&schema_for!(Config), &out_dir);
+    export_schema(&schema_for!(ConfigResponse), &out_dir);
+    export_schema(&schema_for!(TemplateResponse), &out_dir);
+    export_schema(&schema_for!(TemplatesResponse), &out_dir);
 }
