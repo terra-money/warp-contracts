616a6a5d7cf274d98bc683da37ed945120c662ae
simke9445 <simke9445@gmail.com>
2023-09-13 17:34:25 +0200

    schema

--- contracts/warp-controller/examples/warp-controller-schema.rs
+++ contracts/warp-controller/examples/warp-controller-schema.rs
@@ -4,7 +4,7 @@ use std::fs::create_dir_all;
 use controller::{
     account::{AccountResponse, AccountsResponse},
     job::{JobResponse, JobsResponse},
-    QueryMsg, {Config, ConfigResponse, ExecuteMsg, InstantiateMsg},
+    QueryMsg, {Config, ConfigResponse, ExecuteMsg, InstantiateMsg}, State, StateResponse,
 };
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 
@@ -19,6 +19,8 @@ fn main() {
     export_schema(&schema_for!(QueryMsg), &out_dir);
     export_schema(&schema_for!(Config), &out_dir);
     export_schema(&schema_for!(ConfigResponse), &out_dir);
+    export_schema(&schema_for!(State), &out_dir);
+    export_schema(&schema_for!(StateResponse), &out_dir);
     export_schema(&schema_for!(JobResponse), &out_dir);
     export_schema(&schema_for!(JobsResponse), &out_dir);
     export_schema(&schema_for!(AccountResponse), &out_dir);
